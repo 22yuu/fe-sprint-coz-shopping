d9f0e40bdb8b201a7e39ed587d755b65ed5bfa9b
22yuu <22yuu@naver.com>
2023-05-15 14:02:15 +0900

    feat: Footer 컴포넌트 구현 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import Footer from './components/Footer';
 import Header from './components/Header';
 
 export default function App() {
   return (
     <>
       <Header />
+      <main className="flex-1">main</main>
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className="flex flex-col items-center justify-center w-full h-15 p-4 text-xs text-gray-400 line">
+      <p>개인정보 처리방침 | 이용 약관</p>
+      <p>All rights reserved &copy; Codestates</p>
+    </footer>
+  );
+}
