1040b46dbf798b30be6a78abec6885c112bbcd64
22yuu <22yuu@naver.com>
2023-05-16 12:04:02 +0900

    feat(Header, DropDonwnMenu): Link 경로 설정 완료 DropDownMenu 리스트 클릭 시 해당 페이지로 이동

--- src/components/DropDownMenu.tsx
+++ src/components/DropDownMenu.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import bookmark from '../assets/bookmark.svg';
 import product from '../assets/product.svg';
+import { Link } from 'react-router-dom';
 
 export default function DropDownMenu() {
   return (
@@ -8,16 +9,16 @@ export default function DropDownMenu() {
       <ul className="flex flex-col">
         <li className="px-2.5 py-3 cursor-default">OOO님, 안녕하세요!</li>
         <li className="px-2.5 py-3 line">
-          <a href="#">
+          <Link to="productlist">
             <img className="inline mr-2" src={product} alt="상품 아이콘" />
             상품리스트 페이지
-          </a>
+          </Link>
         </li>
         <li className="px-2.5 py-3 line">
-          <a href="#">
+          <Link to="bookmark">
             <img className="inline mr-2" src={bookmark} alt="북마크 아이콘" />
             북마크 페이지
-          </a>
+          </Link>
         </li>
       </ul>
     </div>
