65a8628b78ac65ac436bc72a9b371d85bee15143
22yuu <22yuu@naver.com>
2023-05-18 19:51:13 +0900

    feat: 북마크 페이지 구현 완료

--- src/pages/Bookmark.tsx
+++ src/pages/Bookmark.tsx
@@ -1,5 +1,19 @@
-import React from 'react';
+import Filter from '../components/Filter';
+import ItemList from '../components/ItemList';
+
+import { useOutletContext } from 'react-router-dom';
+import { useBookmark } from '../context/BookmarkContext';
 
 export default function Bookmark() {
-  return <main className="flex-1">Bookmark Page</main>;
+  const { bookmarkList } = useBookmark();
+  const { filter, handleChangeFilter } = useOutletContext();
+
+  return (
+    <main className="flex-1">
+      <section className="w-full mb-2">
+        <Filter filter={filter} onChangeFilter={handleChangeFilter} />
+      </section>
+      <ItemList filter={filter} items={Object.values(bookmarkList)} />
+    </main>
+  );
 }
