30d611c4e2367b45884f125b19bd5af6497f363d
22yuu <22yuu@naver.com>
2023-05-18 15:46:33 +0900

    feat: 상품 목록 페이지 필터 마크업 및 css 완료

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -6,13 +6,15 @@ export default function ItemList({ title, items }) {
   return (
     <section className="w-full mb-10">
       {title && <h2>{title}</h2>}
-      <ul className="grid grid-colos-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {items.length > 0 ? (
-          items.map((item) => <ItemCard key={item.id} item={item} />)
-        ) : (
-          <img className="mx-auto" src={empty} alt="empty image" />
-        )}
-      </ul>
+      {items.length > 0 ? (
+        <ul className="grid grid-colos-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          {items.map((item) => (
+            <ItemCard key={item.id} item={item} />
+          ))}
+        </ul>
+      ) : (
+        <img className="mx-auto" src={empty} alt="empty image" />
+      )}
     </section>
   );
 }
--- src/pages/ProductList.tsx
+++ src/pages/ProductList.tsx
@@ -3,6 +3,12 @@ import { useState, useEffect } from 'react';
 import ItemList from '../components/ItemList';
 import { useProductApi } from '../context/ProductContext';
 
+import filterAll from '../assets/filter-all.png';
+import filterBrand from '../assets/filter-brand.png';
+import filterCategory from '../assets/filter-category.png';
+import filterExhibition from '../assets/filter-exhibition.png';
+import filterProduct from '../assets/filter-product.png';
+
 export default function ProductList() {
   const { productApi } = useProductApi();
   const [products, setProducts] = useState([]);
@@ -13,13 +19,28 @@ export default function ProductList() {
   }, []);
   return (
     <main className="flex-1">
-      <section className="w-full">
-        <ul className="flex justify-center p-4">
-          <li>전체</li>
-          <li>상품</li>
-          <li>카테고리</li>
-          <li>기획전</li>
-          <li>브랜드</li>
+      <section className="w-full mb-2">
+        <ul className="flex gap-10 justify-center p-4">
+          <li>
+            <img src={filterAll} alt="전체" />
+            <p className="text-center">전체</p>
+          </li>
+          <li>
+            <img src={filterProduct} alt="전체" />
+            <p className="text-center">상품</p>
+          </li>
+          <li>
+            <img src={filterCategory} alt="전체" />
+            <p className="text-center">카테고리</p>
+          </li>
+          <li>
+            <img src={filterExhibition} alt="전체" />
+            <p className="text-center">기획전</p>
+          </li>
+          <li>
+            <img src={filterBrand} alt="전체" />
+            <p className="text-center">브랜드</p>
+          </li>
         </ul>
       </section>
       <ItemList items={products} />
